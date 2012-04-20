cb27bf4563dc15d5f42516071ce9599715e24792
abourdon <aurelien.bourdon@inria.fr>
2012-04-20 16:55:41 +0200

    First version of a CPU sensor

--- cpusensor/src/main/scala/powerapi/sensor/cpusensor/CPUSensor.scala
+++ cpusensor/src/main/scala/powerapi/sensor/cpusensor/CPUSensor.scala
@@ -20,16 +20,117 @@ package powerapi.sensor.cpusensor
 import akka.actor.Actor
 import powerapi.core.Configuration
 import powerapi.core.Tick
+import powerapi.core.Process
+import scala.io.Source
+import scala.util.matching.Regex
+import akka.actor.ActorLogging
+import scala.concurrent.ops
+import akka.actor.Props
+import java.io.File
 
-class CPUSensor extends Actor with Configuration {
+/** Messages definition */
+case class TimeInStates(times: Map[Int, Int])
+case class GlobalElapsedTime(time: Int)
+case class ProcessElapsedTime(time: Int)
+case class CPUSensorValues(
+  timeInStates: TimeInStates,
+  globalElapsedTime: GlobalElapsedTime,
+  processElapsedTime: ProcessElapsedTime,
+  tick: Tick)
+
+class CPUSensor extends Actor with Configuration with ActorLogging {
+
+  class Frequency {
+    lazy val timeInStateFiles = {
+      val timesInState = fromConf("timesInState")(elt => (elt \\ "@value").text)(0)
+      val cores = fromConf("cores")(elt => (elt \\ "@value").text.toInt)(0)
+      for (core <- 0 until cores) yield (timesInState replace ("%?", core.toString))
+    }
+    lazy val TimeInStateFormat = """(\d+)\s+(\d+)""".r
+
+    def timeInStates = {
+      val result = collection.mutable.HashMap[Int, Int]()
+
+      // TODO inform when time_in_state file is inaccessible 
+      for {
+        timeInStateFile <- timeInStateFiles
+        file = new File(timeInStateFile)
+        if (file canRead)
+        line <- Source.fromFile(file).getLines
+      } {
+        line match {
+          case TimeInStateFormat(frequency, time) =>
+            result += (frequency.toInt -> (time.toInt + (result getOrElse (frequency.toInt, 0))))
+          case _ => log.warning("unable to parse line \"" + line + "\" from file \"" + timeInStateFile)
+        }
+      }
+
+      result.toMap[Int, Int]
+    }
+  }
+
+  class Time {
+    lazy val globalStatFile = fromConf("globalStat")(elt => (elt \\ "@value").text)(0)
+    lazy val GlobalStatFormat = """cpu\s+([\d\s]+)""".r
+    def globalElapsedTime = {
+      val statFile = new File(globalStatFile)
+      if (statFile canRead) {
+        val line = Source.fromFile(statFile).getLine(0)
+        line match {
+          case GlobalStatFormat(times) => List.fromString(times, ' ').foldLeft(0) { (acc, x) => (acc + x.toInt) }
+          case _ => {
+            log.warning("unable to parse line \"" + line + "\" from file \"" + statFile)
+            -1
+          }
+        }
+      } else {
+        log.warning("unable to read file \"" + statFile + "\"")
+        -1
+      }
+    }
+
+    lazy val processStatFile = fromConf("processStat")(elt => (elt \\ "@value").text)(0)
+    def processElapsedTime(implicit process: Process) = {
+      val statFile = new File(processStatFile.replace("%?", process.pid.toString))
+      if (statFile canRead) {
+        val line = Source.fromFile(statFile).getLine(0) split ("""\s""")
+        // User time + System time + Block IO waiting time
+        line(13).toInt + line(14).toInt + line(41).toInt
+      } else {
+        log.warning("unable to read file \"" + statFile + "\"")
+        -1
+      }
+    }
+
+    def elapsedTime(implicit process: Process = Process(-1)) = {
+      if (process == Process(-1))
+        globalElapsedTime
+      else
+        processElapsedTime
+    }
+  }
+
+  lazy val frequency = new Frequency
+  lazy val time = new Time
+
   def receive = {
     case tick: Tick => process(tick)
   }
 
-  /**
-   * Process according to the given tick
-   */
   def process(tick: Tick) {
+    publish(
+      CPUSensorValues(
+        TimeInStates(timeInStates),
+        GlobalElapsedTime(elapsedTime),
+        ProcessElapsedTime(elapsedTime(tick.subscription.process)),
+        tick))
   }
 
+  def timeInStates = frequency.timeInStates
+
+  def elapsedTime(implicit process: Process = Process(-1)) = time.elapsedTime
+
+  def publish(sensorValues: CPUSensorValues) {
+    context.system.eventStream publish sensorValues
+  }
 }
--- cpusensor/src/test/scala/powerapi/sensor/cpusensor/CPUSensorSuite.scala
+++ cpusensor/src/test/scala/powerapi/sensor/cpusensor/CPUSensorSuite.scala
@@ -20,10 +20,83 @@ package powerapi.sensor.cpusensor
 import org.junit.Test
 import org.scalatest.junit.JUnitSuite
 import org.scalatest.junit.ShouldMatchersForJUnit
+import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+import akka.util.duration.intToDurationInt
+import powerapi.core.Process
+import powerapi.core.Tick
+import powerapi.core.TickSubscription
+import akka.actor.Actor
+import powerapi.core.TickSubscription
+import akka.actor.Props
+import powerapi.core.Clock
+import powerapi.core.Subscribe
+import powerapi.core.Unsubscribe
+
+class CPUSensorReceiver extends Actor {
+  var receivedData: Option[CPUSensorValues] = None
+
+  def receive = {
+    case cpuSensorValues: CPUSensorValues => receivedData = Some(cpuSensorValues)
+  }
+}
 
 class CPUSensorSuite extends JUnitSuite with ShouldMatchersForJUnit {
-  @Test
-  def testMock {
-    1 should equal(1)
+  implicit val system = ActorSystem("cpusensorsuite")
+  implicit val tick = Tick(TickSubscription(Process(123), 1 second))
+  val cpuSensor = TestActorRef[CPUSensor]
+
+  private def testTimeInStates(timeInStates: TimeInStates) {
+    timeInStates.times.size should equal(4)
+    timeInStates.times(4000000) should equal(16)
+    timeInStates.times(3000000) should equal(12)
+    timeInStates.times(2000000) should equal(8)
+    timeInStates.times(1000000) should equal(4)
   }
+
+  @Test
+  def testTimeInStates {
+    testTimeInStates(TimeInStates(cpuSensor.underlyingActor.timeInStates))
+  }
+
+  private def testGlobalElapsedTime(globalElapsedTime: GlobalElapsedTime) {
+    globalElapsedTime.time should equal(441650 + 65 + 67586 + 3473742 + 31597 + 0 + 7703 + 0 + 0 + 0)
+  }
+
+  @Test
+  def testGlobalElapsedTime {
+    testGlobalElapsedTime(GlobalElapsedTime(cpuSensor.underlyingActor.elapsedTime))
+  }
+
+  private def testProcessElapsedTime(processElapsedTime: ProcessElapsedTime) {
+    processElapsedTime.time should equal(2 + 2 + 34)
+  }
+
+  @Test
+  def testProcessElapsedTime {
+    testProcessElapsedTime(ProcessElapsedTime(cpuSensor.underlyingActor.elapsedTime(Process(123))))
+  }
+
+  @Test
+  def testTick() {
+    val cpuSensorReceiver = TestActorRef[CPUSensorReceiver]
+    val clock = system.actorOf(Props[Clock])
+    system.eventStream.subscribe(cpuSensor, classOf[Tick])
+    system.eventStream.subscribe(cpuSensorReceiver, classOf[CPUSensorValues])
+
+    clock ! Subscribe(TickSubscription(Process(123), 10 seconds))
+    Thread.sleep(1000)
+    clock ! Unsubscribe(TickSubscription(Process(123), 10 seconds))
+
+    cpuSensorReceiver.underlyingActor.receivedData match {
+      case None => fail
+      case Some(cpuSensorValues) => {
+        testTimeInStates(cpuSensorValues.timeInStates)
+        testGlobalElapsedTime(cpuSensorValues.globalElapsedTime)
+        testProcessElapsedTime(cpuSensorValues.processElapsedTime)
+      }
+    }
+
+  }
+
 }
