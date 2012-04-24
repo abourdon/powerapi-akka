9e30ed8f96b22b54b306e28f542e7a34fca70c7e
abourdon <aurelien.bourdon@inria.fr>
2012-04-24 13:42:11 +0200

    First version of a CPU formula module

--- cpuformula/src/main/scala/powerapi/formula/cpuformula/CPUFormula.scala
+++ cpuformula/src/main/scala/powerapi/formula/cpuformula/CPUFormula.scala
@@ -1,21 +1,90 @@
+/**
+ * Copyright (C) 2012 Inria, University Lille 1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 package powerapi.formula.cpuformula
-import akka.actor.Actor
-import powerapi.core.Configuration
+import scala.collection.mutable.HashMap
 
-case object Tdp
-case object NumberOfCores
-case object Frequencies
-case class Frequency(value: Int, voltage: Double)
+import akka.actor.Actor
+import akka.util.Duration
+import powerapi.core.Configuration
+import powerapi.core.Energy
+import powerapi.core.Tick
+import powerapi.sensor.cpusensor.CPUSensorValues
+import powerapi.sensor.cpusensor.GlobalElapsedTime
+import powerapi.sensor.cpusensor.ProcessElapsedTime
+import powerapi.sensor.cpusensor.TimeInStates
+
+/** Messages definition */
+case class CPUFormulaValues(energy: Energy, tick: Tick)
 
 class CPUFormula extends Actor with Configuration {
   // Environment specific values (from the configuration file)
   lazy val tdp = fromConf("tdp") { node => (node \\ "@value").text.toDouble }(0)
-  lazy val numberOfCores = fromConf("numberOfCores") { node => (node \\ "@value").text.toInt }(0)
-  lazy val frequencies = fromConf("frequency") { node => Frequency((node \\ "@value").text.toInt, (node \\ "@voltage").text.toDouble) }
+  lazy val cores = fromConf("cores") { elt => (elt \\ "@value").text.toInt }(0)
+  lazy val voltages = fromConf("frequency") { node => ((node \\ "@value").text.toInt, (node \\ "@voltage").text.toDouble) }.toMap
+  lazy val constant = (0.7 * tdp) / (voltages.max._1 * math.pow(voltages.max._2, 2))
+  lazy val powers = voltages.map(voltage => (voltage._1, (constant * voltage._1 * math.pow(voltage._2, 2))))
+
+  lazy val cache = HashMap[Duration, CPUSensorValues]()
+  lazy val defaultSensorValue =
+    CPUSensorValues(
+      TimeInStates(voltages.map(fv => (fv._1, 0))),
+      GlobalElapsedTime(0),
+      ProcessElapsedTime(0),
+      null
+    )
+
+  def process(implicit cpuSensorValues: CPUSensorValues) {
+    publish(compute)
+    refreshCache
+  }
+
+  def usage(old: CPUSensorValues, now: CPUSensorValues): Double =
+    math.max(
+      0.0,
+      math.min(
+        (now.processElapsedTime.time - old.processElapsedTime.time).toDouble / ((now.globalElapsedTime.time - old.globalElapsedTime.time).toDouble / cores),
+        1.0
+      )
+    )
+
+  def power(old: CPUSensorValues, now: CPUSensorValues): Double = {
+    val timeInStates = now.timeInStates - old.timeInStates
+    val totalPower = powers.foldLeft(0: Double) { (acc, power) => acc + (power._2 * timeInStates.times.getOrElse(power._1, 0)) }
+    totalPower / timeInStates.times.foldLeft(0) { (acc, time) => acc + time._2 }
+  }
+
+  def compute(implicit now: CPUSensorValues): CPUFormulaValues = {
+    val old = cache getOrElse (now.tick.subscription.duration, defaultSensorValue)
+
+    val computed = power(old, now) * usage(old, now)
+    CPUFormulaValues(Energy.fromPower(computed), now.tick)
+  }
+
+  def publish(implicit processed: CPUFormulaValues) {
+    context.system.eventStream publish processed
+  }
+
+  def refreshCache(implicit now: CPUSensorValues) {
+    cache += (now.tick.subscription.duration -> now)
+  }
 
   def receive = {
-    case Tdp => tdp
-    case NumberOfCores => numberOfCores
-    case Frequencies => frequencies
+    case cpuSensorValues: CPUSensorValues => process(cpuSensorValues)
   }
 }
--- cpuformula/src/test/scala/powerapi/formula/cpuformula/CPUFormulaSuite.scala
+++ cpuformula/src/test/scala/powerapi/formula/cpuformula/CPUFormulaSuite.scala
@@ -17,13 +17,37 @@
  * Boston, MA  02110-1301, USA.
  */
 package powerapi.sensor.cpusensor
+import java.lang.management.ManagementFactory
+
+import scala.io.Source
+import scala.util.Random
+
 import org.junit.Test
 import org.scalatest.junit.JUnitSuite
 import org.scalatest.junit.ShouldMatchersForJUnit
+
+import akka.actor.actorRef2Scala
+import akka.actor.Actor
+import akka.actor.ActorLogging
 import akka.actor.ActorSystem
+import akka.actor.Props
 import akka.testkit.TestActorRef
+import akka.util.duration.intToDurationInt
+import powerapi.core.Tick
+import powerapi.core.Clock
+import powerapi.core.Configuration
+import powerapi.core.Process
+import powerapi.core.TickIt
+import powerapi.core.TickSubscription
+import powerapi.core.UnTickIt
+import powerapi.formula.cpuformula.CPUFormulaValues
 import powerapi.formula.cpuformula.CPUFormula
-import powerapi.formula.cpuformula.Frequency
+
+class CPUFormulaReceiver extends Actor with ActorLogging {
+  def receive = {
+    case cpuFormulaValues: CPUFormulaValues => log.debug(cpuFormulaValues.energy.toString)
+  }
+}
 
 class CPUFormulaSuite extends JUnitSuite with ShouldMatchersForJUnit {
   implicit val system = ActorSystem("cpuformulasuite")
@@ -31,20 +55,117 @@ class CPUFormulaSuite extends JUnitSuite with ShouldMatchersForJUnit {
 
   @Test
   def testTdp {
-    cpuformula.tdp should equal(42)
+    cpuformula.tdp should equal(105)
   }
 
   @Test
-  def testNumberOfCores {
-    cpuformula.numberOfCores should equal(4)
+  def testCores {
+    cpuformula.cores should equal(4)
   }
 
   @Test
-  def testFrequencies {
-    cpuformula.frequencies.size should equal(3)
-    cpuformula.frequencies(0) should equal(Frequency(1800002, 1.31))
-    cpuformula.frequencies(1) should equal(Frequency(2100002, 1.41))
-    cpuformula.frequencies(2) should equal(Frequency(2400003, 1.5))
+  def testVoltages {
+    cpuformula.voltages.size should equal(3)
+    cpuformula.voltages(1800002) should equal(1.31)
+    cpuformula.voltages(2100002) should equal(1.41)
+    cpuformula.voltages(2400003) should equal(1.5)
+  }
+
+  @Test
+  def testConstant {
+    cpuformula.constant should equal((0.7 * cpuformula.tdp) / (cpuformula.voltages.max._1 * math.pow(cpuformula.voltages.max._2, 2)))
+  }
+
+  @Test
+  def testPowers {
+    cpuformula.powers.size should equal(3)
+    cpuformula.powers.foreach(power => power._2 should equal(
+      cpuformula.constant * power._1 * math.pow(cpuformula.voltages(power._1), 2)
+    ))
+  }
+
+  @Test
+  def testUsage {
+    val old = CPUSensorValues(
+      TimeInStates(Map[Int, Int]()),
+      GlobalElapsedTime(100),
+      ProcessElapsedTime(50),
+      null
+    )
+
+    val now = CPUSensorValues(
+      TimeInStates(Map[Int, Int]()),
+      GlobalElapsedTime(300),
+      ProcessElapsedTime(80),
+      null
+    )
+
+    cpuformula.usage(old, now) should equal((80.0 - 50) / ((300.0 - 100) / cpuformula.cores))
+  }
+
+  @Test
+  def testPower {
+    val oldTimeInStates = TimeInStates(Map[Int, Int](1800002 -> 10, 2100002 -> 20, 2400003 -> 30))
+    val old = CPUSensorValues(
+      oldTimeInStates,
+      GlobalElapsedTime(100),
+      ProcessElapsedTime(50),
+      null
+    )
+
+    val nowTimInStates = TimeInStates(Map[Int, Int](1800002 -> 100, 2100002 -> 200, 2400003 -> 300))
+    val now = CPUSensorValues(
+      nowTimInStates,
+      GlobalElapsedTime(300),
+      ProcessElapsedTime(80),
+      null
+    )
+
+    val diffTimeInStates = nowTimInStates - oldTimeInStates
+    val totalPowers = diffTimeInStates.times.foldLeft(0: Double) { (acc, time) => acc + (cpuformula.powers(time._1) * time._2) }
+    val totalTimes = diffTimeInStates.times.foldLeft(0) { (acc, time) => acc + time._2 }
+
+    cpuformula.power(old, now) should equal(totalPowers / totalTimes)
+  }
+
+  trait ConfigurationMock extends Configuration {
+    val timeInState = "/sys/devices/system/cpu/cpu%?/cpufreq/stats/time_in_state"
+    val frequencies = {
+      val lines = Source.fromFile(timeInState.replace("%?", "1")).getLines
+      (for (line <- lines) yield (line.split("\\s")(0), Random.nextDouble.toString)).map(item => <frequency value={ item._1 } voltage={ item._2 }/>)
+    }
+
+    override lazy val conf =
+      <powerapi>
+        <tdp value="105"/>
+        <cores value="4"/>
+        <globalStat value="/proc/stat/"/>
+        <processStat value="/proc/%?/stat"/>
+        <timesInState value={ timeInState }/>
+        <frequencies>
+          { frequencies }
+        </frequencies>
+      </powerapi>
+  }
+
+  @Test
+  def testAll {
+    val clock = system.actorOf(Props[Clock])
+    val cpuformulaReceiver = system.actorOf(Props[CPUFormulaReceiver])
+    val cpusensor = system.actorOf(Props(new CPUSensor with ConfigurationMock))
+    val cpuFormula = system.actorOf(Props(new CPUFormula with ConfigurationMock))
+
+    system.eventStream subscribe (cpusensor, classOf[Tick])
+    system.eventStream subscribe (cpuFormula, classOf[CPUSensorValues])
+    system.eventStream subscribe (cpuformulaReceiver, classOf[CPUFormulaValues])
+
+    clock ! TickIt(TickSubscription(Process(ManagementFactory.getRuntimeMXBean.getName.split("@")(0).toInt), 1 seconds))
+    Thread.sleep(2000)
+    clock ! TickIt(TickSubscription(Process(ManagementFactory.getRuntimeMXBean.getName.split("@")(0).toInt), 500 milliseconds))
+    Thread.sleep(5000)
+    clock ! UnTickIt(TickSubscription(Process(ManagementFactory.getRuntimeMXBean.getName.split("@")(0).toInt), 500 milliseconds))
+    Thread.sleep(2000)
+    clock ! UnTickIt(TickSubscription(Process(ManagementFactory.getRuntimeMXBean.getName.split("@")(0).toInt), 1 seconds))
   }
 
 }
