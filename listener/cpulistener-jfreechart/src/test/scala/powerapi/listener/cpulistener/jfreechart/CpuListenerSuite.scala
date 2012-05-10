709b2222045370a7df8ab9533243cb176746a64a
abourdon <aurelien.bourdon@inria.fr>
2012-05-10 17:10:39 +0200

    Initial import for powerapi.listener.cpulistener-jfreechart: listener displaying Cpu PowerAPI information in using the JFreeChart library

---
+++ listener/cpulistener-jfreechart/src/test/scala/powerapi/listener/cpulistener/jfreechart/CpuListenerSuite.scala
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2012 Inria, University Lille 1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+package powerapi.listener.cpulistener.jfreechart
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.junit.ShouldMatchersForJUnit
+import org.junit.Test
+
+class CpuListenerSuite extends JUnitSuite with ShouldMatchersForJUnit {
+
+  @Test
+  def testMock {
+    1 should equal(1)
+  }
+
+}
