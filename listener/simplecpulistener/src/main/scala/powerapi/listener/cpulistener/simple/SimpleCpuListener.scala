d25909ce918b0a31c7ea770d4f25da6d6f616ec4
abourdon <aurelien.bourdon@inria.fr>
2012-05-10 16:59:54 +0200

    File naming convention

--- listener/simplecpulistener/src/main/scala/powerapi/listener/cpulistener/simple/SimpleCpuListener.scala
+++ listener/simplecpulistener/src/main/scala/powerapi/listener/cpulistener/simple/SimpleCpuListener.scala
@@ -16,7 +16,7 @@
  * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301, USA.
  */
-package powerapi.listener.simplecpulistener
+package powerapi.listener.cpulistener.simple
 
 import powerapi.formula.cpuformula.CpuFormulaValues
 import powerapi.core.Listener
