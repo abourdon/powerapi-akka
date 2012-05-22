a498841515ca7d58c226c4cdbd00fbae2e37c82b
abourdon <aurelien.bourdon@inria.fr>
2012-05-22 16:19:02 +0200

    Cpu Monitor's intensive process now only keep alive processes to improve accuracy during real time utilization

--- example/cpumonitor/src/main/scala/powerapi/example/cpumonitor/CpuMonitor.scala
+++ example/cpumonitor/src/main/scala/powerapi/example/cpumonitor/CpuMonitor.scala
@@ -20,77 +20,14 @@ package powerapi.example.cpumonitor
 
 import powerapi.powerapi.PowerAPI
 import powerapi.core.Clock
-import java.lang.management.ManagementFactory
 import powerapi.sensor.cpusensor.linux.CpuSensor
 import powerapi.formula.cpuformula.simple.CpuFormula
-import powerapi.core.Process
-import akka.util.duration._
-import powerapi.listener.cpulistener.jfreechart.CpuListener
-import scala.io.Source
-import powerapi.listener.cpulistener.jfreechart.CpuListener
-import powerapi.formula.cpuformula.CpuFormulaValues
-import powerapi.listener.cpulistener.jfreechart.Chart
-
-class CpuMonitor extends CpuListener {
-  val powers = collection.mutable.HashMap[Process, Double]()
-
-  override def listen = {
-    case cpuFormulaValues: CpuFormulaValues => {
-      val process = cpuFormulaValues.tick.subscription.process
-      val old = powers getOrElse (process, Double.PositiveInfinity)
-      val now = cpuFormulaValues.energy.power
-      // We only process the chart if the difference between old and new value are over 10%
-      if (math.min(now, old) / math.max(now, old) < 0.1) {
-        powers += (process -> now)
-        Chart.process(cpuFormulaValues)
-      }
-    }
-  }
-}
+import javax.swing.SwingUtilities
 
 object CpuMonitor {
-  def perso {
-    PowerAPI.startModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
-
-    val currentPid = ManagementFactory.getRuntimeMXBean.getName.split("@")(0).toInt
-    PowerAPI.startMonitoring(Process(currentPid), 500 milliseconds, classOf[CpuListener])
-    PowerAPI.startMonitoring(Process(16617), 500 milliseconds, classOf[CpuListener])
-    Thread.sleep((5 minutes).toMillis)
-    PowerAPI.stopMonitoring(Process(currentPid), 500 milliseconds, classOf[CpuListener])
-    PowerAPI.stopMonitoring(Process(16617), 500 milliseconds, classOf[CpuListener])
-
-    PowerAPI.stopModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
-  }
-
-  def current {
-    PowerAPI.startModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
-
-    val currentPid = ManagementFactory.getRuntimeMXBean.getName.split("@")(0).toInt
-    PowerAPI.startMonitoring(Process(currentPid), 500 milliseconds, classOf[CpuListener])
-    Thread.sleep((5 minutes).toMillis)
-    PowerAPI.stopMonitoring(Process(currentPid), 500 milliseconds, classOf[CpuListener])
-
-    PowerAPI.stopModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
-  }
-
-  def intensive {
-    PowerAPI.startModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
-
-    val PSFormat = """^\s*(\d+).*""".r
-    val pids = Source.fromInputStream(Runtime.getRuntime.exec(Array("ps", "-A")).getInputStream).getLines.toList.map({ pid =>
-      pid match {
-        case PSFormat(pid) => pid.toInt
-        case _ => 1
-      }
-    })
-    pids.foreach(pid => PowerAPI.startMonitoring(Process(pid), 500 milliseconds, classOf[CpuMonitor]))
-    Thread.sleep((5 minutes).toMillis)
-    pids.foreach(pid => PowerAPI.stopMonitoring(Process(pid), 500 milliseconds, classOf[CpuMonitor]))
-
-    PowerAPI.stopModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
-  }
-
   def main(args: Array[String]) {
-    intensive
+    PowerAPI.startModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
+    Processes.intensive
+    PowerAPI.stopModules(Array(classOf[Clock], classOf[CpuSensor], classOf[CpuFormula]))
   }
 }
