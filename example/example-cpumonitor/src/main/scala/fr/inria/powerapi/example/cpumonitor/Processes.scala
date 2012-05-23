80cb297054a431baa84d8926453576ae85417930
abourdon <aurelien.bourdon@inria.fr>
2012-05-23 17:30:51 +0200

    Update license header in following parent last update

--- example/example-cpumonitor/src/main/scala/fr/inria/powerapi/example/cpumonitor/Processes.scala
+++ example/example-cpumonitor/src/main/scala/fr/inria/powerapi/example/cpumonitor/Processes.scala
@@ -15,6 +15,8 @@
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301, USA.
+ *
+ * Contact: powerapi-user-list@googlegroups.com
  */
 package fr.inria.powerapi.example.cpumonitor
 import java.lang.management.ManagementFactory
