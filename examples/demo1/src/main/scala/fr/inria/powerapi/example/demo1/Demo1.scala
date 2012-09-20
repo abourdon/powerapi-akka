071514ec9104d6207f5408a0647db7d1ec65f953
abourdon <aurelien.bourdon@inria.fr>
2012-09-20 15:06:19 +0200

    ADAM evaluation: 3 demos created

---
+++ examples/demo1/src/main/scala/fr/inria/powerapi/example/demo1/Demo1.scala
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2012 Inria, University Lille 1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+ *
+ * Contact: powerapi-user-list@googlegroups.com
+ */
+package fr.inria.powerapi.example.demo1
+import fr.inria.powerapi.library.PowerAPI
+import fr.inria.powerapi.listener.cpudisk.jfreechart.CpuDiskListener
+import fr.inria.powerapi.sensor.cpu.proc.CpuSensor
+import fr.inria.powerapi.formula.cpu.general.CpuFormula
+import fr.inria.powerapi.sensor.disk.proc.DiskSensor
+import fr.inria.powerapi.formula.disk.single.DiskFormula
+import fr.inria.powerapi.core.Process
+import scalax.io.Resource
+import akka.util.duration._
+
+object Demo1 extends App {
+  Array(
+    classOf[CpuSensor],
+    classOf[CpuFormula],
+    classOf[DiskSensor],
+    classOf[DiskFormula]).foreach(PowerAPI.startEnergyModule(_))
+
+  def getPids = {
+    val PSFormat = """^\s*(\d+).*""".r
+    val pids = Resource.fromInputStream(Runtime.getRuntime.exec(Array("ps", "-A")).getInputStream).lines().toList.map({
+      pid =>
+        pid match {
+          case PSFormat(id) => id.toInt
+          case _ => 1
+        }
+    })
+    pids - pids.max
+  }
+
+  val pids = scala.collection.mutable.Set[Int]()
+  val dur = 1 second
+  def udpateMonitoredPids() {
+    val currentPids = scala.collection.mutable.Set[Int](getPids: _*)
+
+    val oldPids = pids -- currentPids
+    oldPids.foreach(pid => PowerAPI.stopMonitoring(process = Process(pid), duration = dur))
+    pids --= oldPids
+
+    val newPids = currentPids -- pids
+    newPids.foreach(pid => PowerAPI.startMonitoring(process = Process(pid), duration = dur))
+    pids ++= newPids
+  }
+
+  PowerAPI.startMonitoring(listenerType = classOf[CpuDiskListener])
+
+  val startingTime = System.currentTimeMillis
+  while (System.currentTimeMillis - startingTime < (2 hours).toMillis) {
+    udpateMonitoredPids()
+    Thread.sleep((250 milliseconds).toMillis)
+  }
+
+  PowerAPI.stopMonitoring(listenerType = classOf[CpuDiskListener])
+
+  Array(
+    classOf[CpuSensor],
+    classOf[CpuFormula],
+    classOf[DiskSensor],
+    classOf[DiskFormula]).foreach(PowerAPI.stopEnergyModule(_))
+}
---
+++ examples/demo3/src/main/scala/fr/inria/powerapi/example/demo3/Demo3.scala
@@ -0,0 +1,51 @@
+/**
+ * Copyright (C) 2012 Inria, University Lille 1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+ *
+ * Contact: powerapi-user-list@googlegroups.com
+ */
+package fr.inria.powerapi.example.demo3
+import com.typesafe.config.ConfigFactory
+import scala.collection.JavaConversions
+import fr.inria.powerapi.sensor.cpu.proc.CpuSensor
+import fr.inria.powerapi.formula.cpu.general.CpuFormula
+import fr.inria.powerapi.sensor.disk.proc.DiskSensor
+import fr.inria.powerapi.formula.disk.single.DiskFormula
+import fr.inria.powerapi.listener.cpudisk.jfreechart.CpuDiskListener
+import fr.inria.powerapi.library.PowerAPI
+import fr.inria.powerapi.core.Process
+import akka.util.duration._
+
+object Demo3 extends App {
+  lazy val conf = ConfigFactory.load
+  lazy val pids = JavaConversions.asScalaBuffer(conf.getIntList("powerapi.pids")).toList
+
+  Array(
+    classOf[CpuSensor],
+    classOf[CpuFormula],
+    classOf[DiskSensor],
+    classOf[DiskFormula]).foreach(PowerAPI.startEnergyModule(_))
+
+  pids.foreach(pid => PowerAPI.startMonitoring(Process(pid), 500 milliseconds, classOf[CpuDiskListener]))
+  Thread.sleep((2 hours).toMillis)
+  pids.foreach(pid => PowerAPI.stopMonitoring(Process(pid), 500 milliseconds, classOf[CpuDiskListener]))
+
+  Array(
+    classOf[CpuSensor],
+    classOf[CpuFormula],
+    classOf[DiskSensor],
+    classOf[DiskFormula]).foreach(PowerAPI.stopEnergyModule(_))
+}
